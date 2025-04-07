2921def55aa214c05bcb58ef0af762b53c02a575
Mitchell Rodrigues <mitch.r.rodrigues@gmail.com>
2025-04-07 00:22:03 -0700

    Oi vey lets make this debug logger go away

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/mitchrodrigues/i2c
+
+go 1.24.1
+
+require github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
--- logger.go
+++ logger.go
@@ -1,11 +1,36 @@
 package i2c
 
-import logger "github.com/d2r2/go-logger"
+import (
+	"sync"
+
+	logger "github.com/d2r2/go-logger"
+)
 
 // You can manage verbosity of log output
 // in the package by changing last parameter value
 // (comment/uncomment corresponding lines).
-var lg = logger.NewPackageLogger("i2c",
-	logger.DebugLevel,
-	// logger.InfoLevel,
+
+type Logger interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+}
+
+var (
+	lg   Logger = logger.NewPackageLogger("i2c", logger.WarnLevel)
+	lock sync.RWMutex
 )
+
+func SetLogger(logger Logger) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	lg = logger
+}
